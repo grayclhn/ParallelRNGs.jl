177ce9bcf98b2cbf3cbe77badcfdd3a3190cfd97
Gray Calhoun <gray@clhn.co>
2014-07-24 13:01:29 -0500

    Add err_retry and err_stop as optional arguments to replicate
    The pmap function has two optional arguments that specify error
    handling. replicate has been using the default values of those
    arguments, but this commit allows them to be set when calling
    replicate. They are still optional arguments and default to the
    old behavior.

--- src/replicate.jl
+++ src/replicate.jl
@@ -1,14 +1,16 @@
 # `replicate` calls `sim` on the output of the `dgp` function `n`
 # times.
 
-function replicate(sim::Function, dgp::Function, n::Integer)
+function replicate(sim::Function, dgp::Function, n::Integer;
+                   err_retry=true, err_stop=false)
     function rvproducer()
         for i = 1:n
             produce(dgp())
         end
     end
 
-    pmap(sim, Task(rvproducer))
+    pmap(sim, Task(rvproducer), err_retry=err_retry, err_stop=err_stop)
 end
 
-replicate(dgp::Function, n::Integer) = replicate(identity, dgp, n)
+replicate(dgp::Function, n::Integer; err_retry=true, err_stop=false) =
+    replicate(identity, dgp, n, err_retry=err_retry, err_stop=err_stop)
